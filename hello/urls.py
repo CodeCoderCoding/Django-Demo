b806ecd45167e6fa4525cb29a932dee8fcc8cde4
codecodercoding <kneil5778@gmail.com>
2021-11-17 12:44:02 +0000

    add form post

--- hello/urls.py
+++ hello/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('articles/<int:year>/', views.year_archive),
     path('articles/<int:year>/<int:month>/', views.month_archive),
     path('articles/<int:year>/<int:month>/<path:title>/', views.article_title),
-    path('articles/search_form/', views.search_form),
+    path('articles/search_form/', views.form_get),
     path('articles/search/', views.search),
-    path('articles/search_post/', views.search_post)
+    path('articles/form_post/', views.form_post)
 ]
 
